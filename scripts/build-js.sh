ef7a7107ba20132f4d4535ddb846aec8870c5780
Felix Wieland <mail@felixwie.com>
2023-05-26 10:39:05 +0200

    build(scripts): added js build script
    a little bash script that builds proto and ts code for js

---
+++ scripts/build-js.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "working from $PWD" 
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=typescript proto/*.proto
+
+pushd typescript > /dev/null || exit 1
+
+# rename proto folder to src
+mv -f proto src
+
+pushd src > /dev/null || exit 1
+
+# remove old index and create new one with all ts files generated
+# rm index.ts 2> /dev/null
+# for e in ./*.ts
+# do
+#   echo "export * from \"$(echo $e | sed 's/.ts//')\"" >> index.ts
+# done
+
+# go back to root
+popd > /dev/null
+popd > /dev/null
+
+npm run build:typescript
